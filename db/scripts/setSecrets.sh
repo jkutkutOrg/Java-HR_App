8a396e60d4a13920a5986f37ca194447ba5c148f
Jkutkut <jorgeregonzalez@gmail.com>
2022-11-24 12:14:26 +0100

    createWebController implemented

--- db/scripts/setSecrets.sh
+++ db/scripts/setSecrets.sh
@@ -33,15 +33,31 @@ askData() {
 }
 
 
-askData "DB_NAME" "HR_App_DB"; DB_NAME="$data"
-askData "DB_PORT" "5847"; DB_PORT="$data"
-askData "DB_USR" "hr"; DB_USR="$data"
-askPassword "$DB_USR"; DB_USR_PASSWD="$PASSWD"
+echo "DB:";
+askData "DB_NAME" "HR_App_DB"; DB_NAME="$data";
+askData "DB_PORT" "5847"; DB_PORT="$data";
+askData "DB_USR" "hr"; DB_USR="$data";
+askPassword "$DB_USR"; DB_USR_PASSWD="$PASSWD";
+
+echo;
+echo "DB Web controller:";
+askData "WEB_CONTROLLER_NAME" "HR_APP_DBController"; WEB_CONTROLLER_NAME="$data";
+askData "WEB_CONTROLLER_PORT" "1250"; WEB_CONTROLLER_PORT="$data";
+askData "WEB_CONTROLLER_EMAIL" "admin@admin.com"; WEB_CONTROLLER_EMAIL="$data";
+askPassword "$WEB_CONTROLLER_EMAIL"; WEB_CONTROLLER_PASSWD="$PASSWD";
 
 echo "#!/bin/bash
 
+# DB
 DB_NAME='$DB_NAME'
 DB_PORT='$DB_PORT'
 DB_USR='$DB_USR'
 DB_USR_PASSWD='$DB_USR_PASSWD'
+
+# DB Web controller
+WEB_CONTROLLER_NAME='$WEB_CONTROLLER_NAME'
+WEB_CONTROLLER_PORT='$WEB_CONTROLLER_PORT'
+WEB_CONTROLLER_EMAIL='$WEB_CONTROLLER_EMAIL'
+WEB_CONTROLLER_PASSWD='$WEB_CONTROLLER_PASSWD'
+
 " > $SECRET_FILE
